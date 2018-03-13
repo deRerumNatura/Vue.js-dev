8a013cc37e910d47a3334bb2122468ba1d35b9d3
dererumnatura <pollindrom@gmail.com>
2018-03-13 14:43:04 +0200

    com

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <my-header></my-header>
+  <div class="container">
+      <products></products>
+      <product-details></product-details>
+  </div>
+</div>
+</template>
+
+<script>
+import ProductsList from './ProductsList.vue'
+import ProductDetails from './ProductDetails.vue'
+import myHeader from './Header.vue'
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    myHeader,
+    'products': ProductsList,
+    ProductDetails
+  }
+}
+</script>
+
+<style>
+  .container{
+    display:flex;
+    align-items:flex-start;
+  }
+</style>
---
+++ src/ProductsList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ProductItem v-for="(product, i) in products" :key="product.id" 
+        :product="product" :index="(i+1)">
+        </ProductItem>
+    </div>
+</template>
+<script>
+    import ProductItem from './ProductItem.vue';
+    import ProductService from  './ProductService.js';
+
+    export default {
+        data() {
+          return {
+              products: []
+          }
+        },
+        components: {
+            ProductItem
+        },
+        created(){
+            this.products = ProductService.products;
+        }
+    }
+</script>
